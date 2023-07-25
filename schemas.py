26dbd147bc471042fc06b0eb23ea0c6cb8dae3ba
Eleonora Belova <eleo.belova@yandex.ru>
2023-07-25 19:46:58 +0200

    Started implementation of sqlalchemy

--- schemas.py
+++ schemas.py
@@ -1,16 +1,26 @@
 from marshmallow import Schema, fields
 
-class ItemSchema(Schema):
+
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
-class StoresSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
 
+class StoreSchema(PlainStoreSchema):
+    items = fields.list(fields.Nested(PlainItemSchema(), dump_only=True))
+
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
