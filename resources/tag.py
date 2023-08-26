777f29a8eca716ae71ec4fef3c641156693bbc49
Eleonora Belova <eleo.belova@yandex.ru>
2023-08-26 19:14:52 +0200

    Add login and logout API requests, handle jwt token creation

--- resources/tag.py
+++ resources/tag.py
@@ -1,38 +1,43 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-
+from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
-from models import StoreModel, TagModel, ItemModel
+from models import TagModel, StoreModel, ItemModel
 from schemas import TagSchema, TagAndItemSchema
 
 blp = Blueprint("Tags", "tags", description="Operations on tags")
 
-@blp.route("/store/<string:store_id>/tag")
+
+@blp.route("/store/<int:store_id>/tag")
 class TagsInStore(MethodView):
     @blp.response(200, TagSchema(many=True))
     def get(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
 
-        return store.tags.all()
+        return store.tags.all()  # lazy="dynamic" means 'tags' is a query
 
     @blp.arguments(TagSchema)
     @blp.response(201, TagSchema)
     def post(self, tag_data, store_id):
         if TagModel.query.filter(TagModel.store_id == store_id, TagModel.name == tag_data["name"]).first():
             abort(400, message="A tag with that name already exists in that store.")
+
         tag = TagModel(**tag_data, store_id=store_id)
+
         try:
             db.session.add(tag)
             db.session.commit()
-        except SQLAlchemyError:
-            abort(500, message=str(e))
+        except SQLAlchemyError as e:
+            abort(
+                500,
+                message=str(e),
+            )
 
         return tag
 
 
-@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+@blp.route("/item/<int:item_id>/tag/<int:tag_id>")
 class LinkTagsToItem(MethodView):
     @blp.response(201, TagSchema)
     def post(self, item_id, tag_id):
@@ -64,22 +69,32 @@ class LinkTagsToItem(MethodView):
 
         return {"message": "Item removed from tag", "item": item, "tag": tag}
 
-blp.route("tag/<string:tag_id>")
+
+@blp.route("/tag/<int:tag_id>")
 class Tag(MethodView):
     @blp.response(200, TagSchema)
     def get(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
         return tag
 
-    @blp.response(202, description="Deletes a tag if no item is tagged with it.", example={"message": "Tag deleted."})
-    @blp.alt_response(400, description="Returned if the tag is assigned to one or more items. In this case, the tag is not deleted.")
+    @blp.response(
+        202,
+        description="Deletes a tag if no item is tagged with it.",
+        example={"message": "Tag deleted."},
+    )
+    @blp.alt_response(404, description="Tag not found.")
+    @blp.alt_response(
+        400,
+        description="Returned if the tag is assigned to one or more items. In this case, the tag is not deleted.",
+    )
     def delete(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
+
         if not tag.items:
             db.session.delete(tag)
             db.session.commit()
             return {"message": "Tag deleted."}
-        abort(400, message="Could not dleete tag. Make sure tag is not associated with any items, then try again.")
-
-
-
+        abort(
+            400,
+            message="Could not delete tag. Make sure tag is not associated with any items, then try again.",  # noqa: E501
+        )
--- schemas.py
+++ schemas.py
@@ -9,35 +9,34 @@ class PlainItemSchema(Schema):
 
 class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-
-
-class PlainTagsSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-
-
-class ItemUpdateSchema(Schema):
     name = fields.Str()
-    price = fields.Float()
-    store_id = fields.Int()
 
 
-class StoreSchema(PlainStoreSchema):
-    items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))
-    tags = fields.List(fields.Nested(PlainTagsSchema(), dump_only=True))
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
-    tags = fields.List(fields.Nested(PlainTagsSchema), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 
-class TagSchema(PlainTagsSchema):
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
-    items = fields.List(fields.Nested(PlainItemSchema), dump_only=True)
 
 
 class TagAndItemSchema(Schema):
